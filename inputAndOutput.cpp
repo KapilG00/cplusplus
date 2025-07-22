f60b78e54f7a495b9c22a892eda8d3570efb683d
kapilg <kapil.gupta4949@gmail.com>
2025-07-22 19:09:13 +0000

    how to take user input and print something

---
+++ inputAndOutput.cpp
@@ -0,0 +1,11 @@
+//#include <iostream>
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    cout << "Value of x is: " << x << " and y is: " << y << endl;
+    return 0;
+}
