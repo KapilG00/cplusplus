32d546f7e089b5d3bffcddf8252f75281e0f1c6e
kapilg <kapil.gupta4949@gmail.com>
2025-07-23 18:05:10 +0000

    datatypes in cpp

---
+++ dataTypes.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+
+    // int
+    int a = 322;
+
+    // long
+    long b = 12000;
+
+    // long long
+    long long c = 140000000;
+    
+    // float
+    float d = 3.14;
+
+    // double
+    double e = 9.8123;
+    
+    // string and getline
+    string f;
+    string g;
+
+    cin >> f >> g;
+    cout << f << " " << g << endl;
+    
+    string h;
+    getline(cin, h);
+    cout << h << endl;
+    
+    // char
+    char i = 'k';
+    cout << i << endl;
+    
+     
+
+  
+    return 0;
+
+}
